8808b0cf75f45419ea530a5367c118e6ba05882f
kurotakazuki <kabisakanakabi@gmail.com>
2023-06-14 19:42:24 +0900

    feat: voicevox api

---
+++ lib/apis/voicevox.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:typed_data';
+import 'package:audioplayers/audioplayers.dart';
+
+class VoiceVoxConnection {
+  // final String _voiceVoxUrl = 'http://127.0.0.1:50021';
+  final String _voiceVoxUrl = 'http://10.0.2.2:50021';
+  final int _speaker;
+
+  VoiceVoxConnection(this._speaker);
+
+  Future<Uint8List> textToAudioClip(String text) async {
+    var queryJson = await sendAudioQuery(text);
+    var clip = await getAudioClip(queryJson);
+    final player = AudioPlayer();
+    await player.play(BytesSource(clip));
+    return clip;
+  }
+
+  Future<String> sendAudioQuery(String text) async {
+    var url = '$_voiceVoxUrl/audio_query?text=$text&speaker=$_speaker';
+    var response = await http.post(Uri.parse(url));
+    if (response.statusCode >= 400) {
+      throw Exception('Error: ${response.statusCode}');
+    }
+    return response.body;
+  }
+
+  Future<Uint8List> getAudioClip(String queryJson) async {
+    var url = '$_voiceVoxUrl/synthesis?speaker=$_speaker';
+    var response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: queryJson,
+    );
+    if (response.statusCode >= 400) {
+      throw Exception('Error: ${response.statusCode}');
+    }
+    return response.bodyBytes;
+  }
+
+  // Future<void> playAudioClip(String queryJson) async {
+  //   var url = '$_voiceVoxUrl/synthesis?speaker=$_speaker';
+  //   final player = AudioPlayer();
+  //   await player.play(UrlSource('https://example.com/my-audio.wav'));
+  // }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
+import './apis/voicevox.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter VOICEVOX Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'VOICEVOX with Flutter'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
-
   final String title;
 
   @override
@@ -34,46 +35,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+  VoiceVoxConnection vv = VoiceVoxConnection(1);
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
-      body: Center(
-
+      body: const Center(
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: <Widget>[],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () async {
+          var audio = await vv.textToAudioClip("こんにちずんずん　ずんだめんなのだ");
+          print(audio.lengthInBytes);
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
