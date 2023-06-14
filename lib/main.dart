cca8e4b694b605355bfab2be1c1c15d664e8b6d3
kurotakazuki <kabisakanakabi@gmail.com>
2023-06-14 19:57:40 +0900

    add: migrate gptChats and audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import './apis/voicevox.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'providers/gpt_chats.dart';
+import './models/gpt_chat.dart';
 
 void main() {
   runApp(
@@ -21,38 +23,43 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'VOICEVOX with Flutter'),
+      home: MyHomePage(title: 'VOICEVOX with Flutter'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends ConsumerWidget {
+  MyHomePage({super.key, required this.title});
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   VoiceVoxConnection vv = VoiceVoxConnection(1);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final gptChatsProvider = ref.watch(gptChatsNotifierProvider);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            Column(
+              children: gptChatsProvider.map((e) => Text(e.chatText)).toList(),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          var audio = await vv.textToAudioClip("こんにちずんずん　ずんだめんなのだ");
+          final gptChatsNotifier = ref.read(gptChatsNotifierProvider.notifier);
+          gptChatsNotifier
+              .addChat(const GptChat(chatText: 'こんにちずんずん　ずんだめんなのだ'));
+          final gptChatsProvider = ref.read(gptChatsNotifierProvider);
+          var audio = await vv.textToAudioClip(gptChatsProvider.last.chatText);
           print(audio.lengthInBytes);
         },
         tooltip: 'Increment',
