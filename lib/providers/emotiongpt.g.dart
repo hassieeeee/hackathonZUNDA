8d7eb098f252a40d814f4fe951a175d23985a641
hassieeeee <tackt_84@au.com>
2023-06-16 02:23:10 +0900

    emotion chatGPT add

---
+++ lib/providers/emotiongpt.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'emotiongpt.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$emotiongptNotifierHash() =>
+    r'aea20b4ff688b520cb6a88c23eb056f0027ecfca';
+
+/// See also [EmotiongptNotifier].
+@ProviderFor(EmotiongptNotifier)
+final emotiongptNotifierProvider =
+    AutoDisposeNotifierProvider<EmotiongptNotifier, List<GPTChat>>.internal(
+  EmotiongptNotifier.new,
+  name: r'emotiongptNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$emotiongptNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EmotiongptNotifier = AutoDisposeNotifier<List<GPTChat>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
